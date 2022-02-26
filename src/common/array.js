83aa954fd7f313778f1d739842793f0eadf2133a
dono118 <dono118@126.com>
2022-02-26 17:38:22 +0800

    侦测数组

--- src/common/array.js
+++ src/common/array.js
@@ -18,6 +18,26 @@ sevenMethods.forEach(methodName => {
     arrayMethods,
     methodName,
     function () {
+      // obj.g是数组，在第一次遍历obj的时候，已经给g属性数组添加了__ob__属性
+      const ob = this.__ob__
+      // push,unshift,splice这三个方法会插入新项，现在要把插入的新项也要被observe
+      let insertedItems = []
+      switch (methodName) {
+        case 'push':
+        case 'unshift':
+          insertedItems = arguments
+          break
+        case 'splice':
+          insertedItems = arguments.slice(2)
+          break
+
+        default:
+          break
+      }
+      // 判断有新插入的项，如果有让其也变为响应式的
+      if (insertedItems.length) {
+        ob.observeArray(insertedItems)
+      }
       // 恢复原来的功能
       // console.log('将要改写7个数组方法。。。')
       original.apply(this, arguments)
